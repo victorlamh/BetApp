be75cc018bbd94ca81621a7a631bbddaf2fa8489
Victor Lamache <victor.lamache@artfx.fr>
2026-05-10 09:59:20 +0200

    Fix type casting in detail.php to resolve SwiftUI decoding errors

--- public_html/api/bets/detail.php
+++ public_html/api/bets/detail.php
@@ -19,17 +19,39 @@ $bet = $db->fetchOne(
 
 if (!$bet) Response::notFound("Bet not found");
 
+// Cast numeric types for Swift
+$bet['id'] = (int)$bet['id'];
+$bet['creator_user_id'] = (int)$bet['creator_user_id'];
+$bet['is_boosted'] = (int)$bet['is_boosted'];
+$bet['proof_required'] = (int)$bet['proof_required'];
+if (isset($bet['result_outcome_id'])) $bet['result_outcome_id'] = (int)$bet['result_outcome_id'];
+
 $bet['outcomes'] = $db->fetchAll(
     "SELECT id, label, initial_coefficient, total_wagered FROM bet_outcomes WHERE bet_id = ? ORDER BY sort_order",
     [$betId]
 );
 
 $bet['outcomes'] = Odds::calculate($betId, $bet['outcomes']);
+foreach ($bet['outcomes'] as &$outcome) {
+    $outcome['id'] = (int)$outcome['id'];
+}
 
-$bet['my_wager'] = $db->fetchOne(
+$wager = $db->fetchOne(
     "SELECT * FROM wagers WHERE bet_id = ? AND user_id = ?",
     [$betId, $userId]
 );
+
+if ($wager) {
+    $wager['id'] = (int)$wager['id'];
+    $wager['bet_id'] = (int)$wager['bet_id'];
+    $wager['outcome_id'] = (int)$wager['outcome_id'];
+    $wager['stake'] = (float)$wager['stake'];
+    $wager['locked_coefficient'] = (float)$wager['locked_coefficient'];
+    $wager['potential_return'] = (float)$wager['potential_return'];
+    $bet['my_wager'] = $wager;
+} else {
+    $bet['my_wager'] = null;
+}
 
 // If result proposed, fetch proposal and votes
 if ($bet['status'] === 'result_proposed') {

66c7b457a5c50dd70f393e85d95da0880f5f24b0
Victor Lamache <victor.lamache@artfx.fr>
2026-05-10 11:45:27 +0200

    Add database inspection script to debug follows table schema

---
+++ public_html/api/inspect_db.php
@@ -0,0 +1,17 @@
+<?php
+require_once __DIR__ . '/bootstrap.php';
+Auth::requireAuth();
+
+$db = DB::getInstance();
+$tables = ['users', 'follows', 'notifications', 'wallets', 'bets'];
+$info = [];
+
+foreach ($tables as $table) {
+    try {
+        $info[$table] = $db->fetchAll("DESCRIBE $table");
+    } catch (Exception $e) {
+        $info[$table] = "Error: " . $e->getMessage();
+    }
+}
+
+Response::success($info);
